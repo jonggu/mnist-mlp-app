f1aa711a5be0af6dbac06224653bb001776a6731
jonggu <jonggukang@hotmail.com>
2020-11-26 10:29:11 +0900

    E29: 1st submission

--- mnist_mlp.py
+++ mnist_mlp.py
@@ -5,6 +5,24 @@ Gets to 98.40% test accuracy after 20 epochs
 2 seconds per epoch on a K520 GPU.
 '''
 
+import numpy as np
+from keras.models import load_model
+from keras.preprocessing import image
+
+def load_keras_model():
+    model = load_model('models') # from ./models directory
+    return model
+
+def predict_number(model, img, width, height):
+    test_image = image.img_to_array(img)
+    test_image = test_image.astype('float32')
+    test_image = test_image.reshape(width, height)
+    test_image /= 255
+    test_image = test_image.reshape(1, width * height)
+    result = model.predict(test_image, batch_size=1)
+    return np.argmax(result)
+
+'''
 from __future__ import print_function
 
 import keras
@@ -79,3 +97,4 @@ test_image /= 255
 test_image = test_image.reshape(1, img_width * img_height)
 result = model.predict(test_image, batch_size=1)
 print(np.argmax(result))
+'''
